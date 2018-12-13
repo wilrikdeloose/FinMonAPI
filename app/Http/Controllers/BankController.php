a635517254ac923b0c089cb6aee1cb4a823c3338
Wilrik <wilrik@wilrik.nl>
2018-12-13 23:54:12 +0100

    heroku + postgres test

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -8,7 +8,7 @@ class BankController extends Controller
 {
     public function test()
     {
-        return response()->json(['all' => 1], 200);
+        return response()->json(['all' => 2], 200);
     }
 
     /**
@@ -18,10 +18,14 @@ class BankController extends Controller
      */
     public function count()
     {
-        $result = app('db')->select("SELECT count(*) as numberOfBanks FROM bank");
+        $dbh = new PDO("postgres://fkujkbucvxqjxj:13221ec17908bc3b474605a583f6a9017266a70068a7f774f32fdebf4c5338a6@ec2-54-75-231-156.eu-west-1.compute.amazonaws.com:5432/d928ssskcmp00m");
+        $stmt = $this->pdo->query("SELECT count(*) as numberOfBanks FROM bank");
+
+        //$result = app('db')->select("SELECT count(*) as numberOfBanks FROM bank");
+        $result = $stmt->fetch();
         return response()->json(['banks' => $result[0]->numberOfBanks], 200);
     }
-    
+
     public function all()
     {
         try
